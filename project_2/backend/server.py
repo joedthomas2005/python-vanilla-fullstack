67190879d10c98f45b35861562a8f58d1e4da461
joe <joedt100@gmail.com>
2022-04-13 20:56:44 +0100

    basic frontend backend communication and also responsive frontend media queries

--- project_2/backend/server.py
+++ project_2/backend/server.py
@@ -16,3 +16,28 @@ while True:
     print("new request")
     request = httpClasses.httpRequest(data)
     print(f"{request.method} REQUEST FOR {request.resource} WITH PARAMS {request.params}")
+
+    if request.method == "GET":
+        if request.resource == "search":
+
+            try:
+                queryTerm = request.params["query"]
+                count = int(request.params["count"])
+
+                items = []
+                for i in range(count):
+                    items.append({
+                        "number": i,
+                        "query": queryTerm
+                    })
+                
+                response = httpClasses.httpResponse("text/html", 200, json.dumps(items))
+                response.setHeader("Access-Control-Allow-Origin","*")
+            except:
+                response = httpClasses.httpResponse("text/html", 400)
+            
+        else:
+            response = httpClasses.httpResponse("text/html", 404)
+        
+
+        c.send(response.build().encode())
--- project_2/frontend/resources/index.js
+++ project_2/frontend/resources/index.js
@@ -1,10 +1,16 @@
 function load(){
-    document.getElementById("search").addEventListener("keyup", (event) =>{
+    document.getElementById("searchBar").addEventListener("keyup", (event) =>{
         if(event.key == "Enter"){
-            let query = document.getElementById("search").value;
+            let query = document.getElementById("searchBar").value;
             console.log(`search query ${query}`);
-            queryBackend(query, 1);
+            queryBackend(query, 10);
         }
+    })
+
+    document.getElementById("searchButton").addEventListener("click", () =>{
+        let query = document.getElementById("searchBar").value;
+        console.log(`search query ${query}`);
+        queryBackend(query, 10);
     })
 }
 
@@ -18,10 +24,11 @@ function queryBackend(search, count){
     request.onreadystatechange = function(){
         if(request.readyState == 4){
             let data = request.responseText;
+            console.log(data);
             results = JSON.parse(data);
-            results.forEach((result) => {
-                console.log(result);
-            })
+             results.forEach((result) => {
+                 console.log(result);
+             })
         }
     }
 }
